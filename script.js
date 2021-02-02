ecda18488534b5975c1f1d0284a11578f2a429f7
MichelBreucklemier <78418038+MichelBreucklemier@users.noreply.github.com>
2021-02-02 06:36:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //is it wright or wrong for blue:
 function choice1(id) {
     var x = " " // correct or incorrect:
-    document.location = "/pe/" //<--- next question
+    document.location = "/" //<--- next question / what file to go next
     alert(x);
 }
 //is it wright or wrong for red:
 function choice2() {
         var x = " " // correct or incorrect:
-          document.location = "/pe/" //<--- next question
+          document.location = "/" //<--- next question / what file to go next
     alert(x);
 }
