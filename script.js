aacc4744162ab93b29b0ac7b4dd392df1cf7fe4e
MichelBreucklemier <78418038+MichelBreucklemier@users.noreply.github.com>
2021-02-02 06:22:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+//is it wright or wrong for blue:
+function choice1(id) {
+    var x = " " // correct or incorrect:
+    document.location = "/pe/" //<--- next question
+    alert(x);
+}
+//is it wright or wrong for red:
+function choice2() {
+        var x = " " // correct or incorrect:
+          document.location = "/pe/" //<--- next question
+    alert(x);
+}
